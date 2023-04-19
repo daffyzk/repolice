906c1fa9bc122690703da9ed89c77a079267a556
maurogarciac <maurogarciacoto@gmail.com>
2023-04-19 14:16:38 -0300

    added script that runs find command for all git repos

--- repo_hub/src/main.rs
+++ repo_hub/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::path::PathBuf;
 use std::io::Result;
+use std::process::{Command, Output};
 
 fn main() {
     let start_dir : Result<PathBuf> = env::current_dir();
@@ -13,5 +14,20 @@ fn main() {
     println!("this is the cwd: {}", start_dir.expect("REASON").display());
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+    get_repos();
+}
 
+fn get_repos() {
+    let cwd_s : String = env::current_dir().unwrap().into_os_string().into_string().unwrap(); 
+    let output : Output = Command::new("find").arg(cwd_s)
+        .arg("-name")
+        .arg(".git")
+        .arg("-type")
+        .arg("d")
+        .output().expect("Error!");
+    let repo_dirs : String = String::from_utf8(output.stdout).unwrap();
+    let repo_list : Vec<&str> = repo_dirs.lines().collect();
+    println!("{:?}", repo_dirs);
+    println!("{:?}", repo_list);
+    
 }
