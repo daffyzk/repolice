ea014af5008ad24594bb22eb4b440bb64e039657
maurogarciac <maurogarciacoto@gmail.com>
2023-04-20 16:38:13 -0300

    fixed match statement, improved repolist getter

--- repo_hub/src/main.rs
+++ repo_hub/src/main.rs
@@ -6,11 +6,11 @@ use std::process::{Stdio, Command, Output};
 fn main() {
 
     let args: Vec<String> = env::args().collect();
-    
-    match args.len() as i64{
+    let arg_len = args.len().to_string().parse::<i32>().unwrap() - 1;
+    match arg_len{
         0 => println!("no args"),
-        1 => println!("args: {}", &args[0]),
-        2 => println!("args: {} - {}", &args[0], &args[1]),
+        1 => println!("args: {}", &args[1]),
+        2 => println!("args: {} - {}", &args[1], &args[2]),
         3 => println!("too many args!"),
         _ => println!("todo"),
     }
@@ -20,16 +20,14 @@ fn main() {
 }
 
 fn get_repos() {
-    let cwd_s : String = get_cwd().unwrap().into_os_string().into_string().unwrap(); 
-    println!("{}", cwd_s);
-    let output : Output = Command::new("find").arg(".")
-        .arg("-name .git")
-        .arg("-type d")
+    let cwd_s : String = get_cwd().unwrap().into_os_string().into_string().unwrap();
+    let output : Output = Command::new("find")
+        .args([&cwd_s,"-name", ".git","-type", "d"])
         .stdout(Stdio::piped())
         .output().expect("Error!");
-    let repo_dirs : String = String::from_utf8_lossy(&output.stdout).to_string();
-    let repo_list : Vec<&str> = repo_dirs.lines().collect();
-    println!("{:?}", repo_dirs);
+    let repo_results : String = String::from_utf8_lossy(&output.stdout).to_string();
+    let repo_list : Vec<&str> = repo_results.lines().collect();
+    //repo_list.iter().filter();
     println!("----list of repositories----");
     println!("{:?}", repo_list);
     
