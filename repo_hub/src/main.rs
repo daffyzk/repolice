8b2335879071def5fb4cb06c3b5162a992ce6ea1
maurogarciac <maurogarciacoto@gmail.com>
2023-04-20 14:41:42 -0300

    used match for args

--- repo_hub/src/main.rs
+++ repo_hub/src/main.rs
@@ -4,30 +4,37 @@ use std::io::Result;
 use std::process::{Command, Output};
 
 fn main() {
-    let start_dir : Result<PathBuf> = env::current_dir();
 
     let args: Vec<String> = env::args().collect();
     
-    let query = &args[1];
-    let file_path = &args[2];
+    match args.len() as i64{
+        0 => println!("no args"),
+        1 => println!("args: {}", &args[0]),
+        2 => println!("args: {} - {}", &args[0], &args[1]),
+        3 => println!("too many args!"),
+        _ => println!("todo"),
+    }
     
-    println!("this is the cwd: {}", start_dir.expect("REASON").display());
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("this is the cwd: {}", get_cwd().expect("REASON").display());
     get_repos();
 }
 
 fn get_repos() {
-    let cwd_s : String = env::current_dir().unwrap().into_os_string().into_string().unwrap(); 
+    let cwd_s : String = get_cwd().unwrap().into_os_string().into_string().unwrap(); 
+    println!("{}", cwd_s);
     let output : Output = Command::new("find").arg(cwd_s)
-        .arg("-name")
-        .arg(".git")
-        .arg("-type")
-        .arg("d")
+        .arg("-name .git")
+        .arg("-type d")
         .output().expect("Error!");
     let repo_dirs : String = String::from_utf8(output.stdout).unwrap();
     let repo_list : Vec<&str> = repo_dirs.lines().collect();
     println!("{:?}", repo_dirs);
+    println!("----list of repositories----");
     println!("{:?}", repo_list);
     
+
+}
+
+fn get_cwd() -> Result<PathBuf>{
+    return env::current_dir();
 }
